3603e4367e59116db2c5c03a427404a6308a207a
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-19 15:25:31 +0000

    init

---
+++ integer.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Oct 2022
+// This is the guessing game program
+
+#include <iostream>
+
+
+int main() {
+    // this function checks if a number is positive or negative
+
+    // input
+    int userInput
+
+    // process
+    if (userInput == "red") {
+        std::cout << "Stop!";
+    } else if (colourOfLight == "yellow") {
+        std::cout << "Slow Down.";
+    } else if (colourOfLight == "green") {
+        std::cout << "Go, if all clear.";
+    } else {
+        std::cout << "No idea!";
+    }
+}
